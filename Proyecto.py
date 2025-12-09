62155e327da585f67e48a621f2dc51f931e2ec99
William159753 <chonatawilliam@gmail.com>
2025-12-09 07:06:24 -0500

    Refactor loan calculation and payment handling

--- Proyecto.py
+++ Proyecto.py
@@ -1,17 +1,53 @@
-Usuario = input("ingresar el nombre del usuario: ")
-Monto = input("Ingrese el monto a cobrar: ")
-from datetime import date
-from dateutil import relativedelta
-Fecha_prestamo = (input("Ingrese la fecha del prestamo(DD-MM-YYYY): "))
-Fecha = date.strftime(Fecha_prestamo, "%d-%m-%Y")
-Numero_pagos = input ("Cuantos pagos va a realizar: ")
+Usuario = input("ingresar el nombre del usuario: ").lower()
+Monto = float(input("Ingrese el monto a cobrar: "))
+from datetime import datetime
+fecha = input("Ingrse la fecha inicial: ")
+fecha_inicial = datetime.strptime(fecha, "%d/%m/%Y")
+dia_paga = fecha_inicial.day
+mes_paga = fecha_inicial.month + 1
+año_paga = fecha_inicial.year
+if mes_paga > 12:
+    mes_paga = 1
+    año_paga += 1
+Gastos_administrativos =float (Monto * 0.005)
+Numero_pagos = int(input ("Cuantos pagos va a realizar: "))
 Tnominal_anual = 0.04
-#Gastos_administrativos =float (Monto * 0.005)
-Multa= float (Tnominal_anual/360)
+Multa= float (Tnominal_anual/360)   
 Tnominal_mensual =float (Tnominal_anual/12)
 
 #Pago 1
-Fecha_pagar= (Fecha + relativedelta(months=1))
-print(Fecha_pagar) 
+for i in range (Numero_pagos):
+    
+    try:
+        print (dia_paga,"/",mes_paga,"/",año_paga)
+        
+        mes_paga += 1  
+        
+        if mes_paga > 12:
+            mes_paga = 1      
+            año_paga += 1     
 
-prueba_si_funciona.
+    except NameError:
+        print("Escribir la fecha como (dd/mm/yyyy)")
+Factor_descuento = (1 + Tnominal_mensual) ** (-Numero_pagos)
+Cuota_fija = (Tnominal_mensual * Monto) / (1 - Factor_descuento)
+DIVIDENDO = Cuota_fija
+
+for i in range(Numero_pagos):
+    fecha_pagar = datetime(año_paga, mes_paga, dia_paga)
+    fecha_rea = input("Ingrese la fecha en que pagó (dd/mm/yyyy): ")
+    fecha_real = datetime.strptime(fecha_rea, "%d/%m/%Y")
+    dia_real = fecha_real.day
+    mes_real = fecha_real.month
+    año_real = fecha_real.year
+    monto_pagado = float(input("Ingrese cuánto pagó este mes: "))
+    if Monto != monto_pagado:
+        while monto_pagado != (Numero_pagos):
+            fecha_pagar = datetime(año_paga, mes_paga, dia_paga)
+            fecha_real = input("Ingrese la fecha en que pagó (dd/mm/yyyy): ")
+            days=0
+            monto_pagado = float(input("Ingrese cuánto pagó este mes: "))
+    else:
+        print ("Usted ha saldado su deuda")
+
+            
